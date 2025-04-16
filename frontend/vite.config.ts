9f190a44ebb720b2e12b4057685806c5375a8ad6
Denys Vuika <denys.vuika@gmail.com>
2025-04-16 15:15:53 -0400

    update vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // 👈 relative paths so Go can serve from root
+  build: {
+    outDir: 'dist',        // default, but just to be explicit
+    emptyOutDir: true,     // cleans before build
+  },
 })
